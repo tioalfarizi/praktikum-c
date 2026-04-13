e7f952ff16f84b7f48f532b7449075ddb99b2644
Tio al farizi <tioalparizi5781@gmail.com>
2026-04-13 21:19:17 +0700

    first commit

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+
+   printf("Hello World\n");
+   printf("Selamat datang di praktikum Pemrograman Dasar\n");
+   printf("Selamat datang di Matkul Pemrograman Dasar\n");
+   
+   return 0;
+}
